3239a42623b8202bde7108815898f680bb23994b
unknown <c6-std14@DSHEILC103955.edu.bd>
2025-07-31 16:23:08 +0600

    first commit

---
+++ src/test/java/com/brainstation_23/LibraryManagementSystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainstation_23.LibraryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
