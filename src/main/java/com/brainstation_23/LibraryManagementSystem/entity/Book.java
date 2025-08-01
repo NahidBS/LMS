d4b545fb7138713b33301ae1a4bd74c99847f650
unknown <c6-std14@DSHEILC103955.edu.bd>
2025-08-01 13:45:21 +0600

    added entity

---
+++ src/main/java/com/brainstation_23/LibraryManagementSystem/entity/Book.java
@@ -0,0 +1,48 @@
+package com.brainstation_23.LibraryManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private category category;
+
+    private String name;
+    private String author;
+
+    private String shortDescription;
+    private int totalCopies = 0;
+    private int availableCopies = 0;
+
+    private String bookCover; //url or file path
+    private String pdfFile;
+    private String audioFile;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = updatedAt = LocalDateTime.now();
+    }
+
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/brainstation_23/LibraryManagementSystem/entity/User.java
@@ -0,0 +1,56 @@
+package com.brainstation_23.LibraryManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    public enum Role{
+        ADMIN, USER
+    }
+
+    private String dateOfBirth;
+    private String address;
+    private String profilePicture;
+
+    private LocalDateTime emailVerifiedAt;
+
+    private String rememberToken;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
+
+
+}
